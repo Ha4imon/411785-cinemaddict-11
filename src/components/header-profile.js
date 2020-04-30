1f84ddd5e69d01078a97322f8c9a5bb28fa31d78
Ha4imon <v1227873@yandex.ru>
2020-04-30 16:21:42 +0500

    Личный проект: шаблонизируй это

--- src/components/header-profile.js
+++ src/components/header-profile.js
@@ -1,8 +1,10 @@
-const createHeaderProfile = () => {
+const createHeaderProfile = (user) => {
+  const {rank} = user;
+
   return (
     `
       <section class="header__profile profile">
-        <p class="profile__rating">Movie Buff</p>
+        <p class="profile__rating">${rank}</p>
         <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
       </section>
     `
--- src/main.js
+++ src/main.js
@@ -4,38 +4,82 @@ import {createSort} from './components/sort';
 import {createFilmsContainer} from './components/film-container';
 import {createFilmTemplate} from './components/film-template';
 import {createBtn} from './components/btn';
+import {createCountFilm} from './components/footer';
 import {createFilmDetails} from './components/film-details';
+import {getFilms} from './mock/films';
+import {getUser} from './mock/user';
 
 const render = (container, el, place = `beforeend`) => {
   container.insertAdjacentHTML(place, el);
 };
 
+const renderFilmTemplate = (start, count, arr, container) => {
+  for (let i = start; i < start + count; i++) {
+    if (i >= arr.length) {
+      break;
+    }
+    render(container, createFilmTemplate(arr[i]));
+  }
+};
+
 const header = document.querySelector(`.header`);
 const main = document.querySelector(`.main`);
+const user = getUser();
+const films = getFilms();
+const watchlistFilms = [];
+const watchedFilms = [];
+const favoritesFilms = [];
 
-render(header, createHeaderProfile());
-render(main, createMenu());
+films.map((film) => {
+  if (film.watchlist) {
+    watchlistFilms.push(film);
+  }
+  if (film.watched) {
+    watchedFilms.push(film);
+  }
+  if (film.favorites) {
+    favoritesFilms.push(film);
+  }
+});
+
+render(header, createHeaderProfile(user));
+render(main, createMenu(watchlistFilms.length, watchedFilms.length, favoritesFilms.length));
 render(main, createSort());
 render(main, createFilmsContainer());
 
 const filmListContainer = document.querySelector(`.films-list`);
 const filmListWrapper = filmListContainer.querySelector(`.films-list__container`);
 
-const FILM_COUNT = 5;
-for (let i = 0; i < FILM_COUNT; i++) {
-  render(filmListWrapper, createFilmTemplate());
-}
+const FILM_COUNT_RENDER = 5;
+let startRenderFilm = 0;
+
+renderFilmTemplate(startRenderFilm, FILM_COUNT_RENDER, films, filmListWrapper);
+startRenderFilm += FILM_COUNT_RENDER;
 
 render(filmListContainer, createBtn());
 
 const filmExtraListContainer = document.querySelectorAll(`.films-list--extra`);
 const FILM_EXTRA_COUNT = 2;
+const START_FILM_EXTRA_COUNT = 0;
 
 filmExtraListContainer.forEach((container) => {
   const filmExtraListWrapper = container.querySelector(`.films-list__container`);
-  for (let i = 0; i < FILM_EXTRA_COUNT; i++) {
-    render(filmExtraListWrapper, createFilmTemplate());
-  }
+  renderFilmTemplate(START_FILM_EXTRA_COUNT, FILM_EXTRA_COUNT, films, filmExtraListWrapper);
 });
 
-render(document.body, createFilmDetails());
+const footerContainer = document.querySelector(`.footer__statistics`);
+
+render(footerContainer, createCountFilm(films.length));
+
+// render(document.body, createFilmDetails(films[0]));
+
+const loadMoreFilmBtn = filmListContainer.querySelector(`.films-list__show-more`);
+
+loadMoreFilmBtn.addEventListener(`click`, function () {
+  renderFilmTemplate(startRenderFilm, FILM_COUNT_RENDER, films, filmListWrapper);
+  startRenderFilm += FILM_COUNT_RENDER;
+
+  if (startRenderFilm >= films.length) {
+    loadMoreFilmBtn.remove();
+  }
+});
